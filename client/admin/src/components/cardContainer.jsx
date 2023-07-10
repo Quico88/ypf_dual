9f2470304cc12c41f089b2039f20451ebe01bdc9
juandrewanz <juan@italic.com>
2023-07-10 13:56:54 -0300

    refactoring

---
+++ client/admin/src/components/cardContainer.jsx
@@ -0,0 +1,21 @@
+import DeviceCard from "./deviceCard";
+
+const CardContainer = ()=> {
+
+    const devices = [ { name: "Lavadero" , url: "carwash"}, { name: "Lavadero" , url: "carwash"}, { name: "Lavadero" , url: "carwash"}, { name: "Lavadero" , url: "carwash"}]
+
+
+    return (
+        <div className='flex flex-wrap justify-center items-center h-full w-full'>
+            {devices.map((device, index)=> 
+               <DeviceCard
+                key={index}
+                name={device.name}
+                url={device.url}
+               />
+            )}
+        </div>
+    )
+}
+
+export default CardContainer;
