391b1b5a4a897b18f98544f4ce8a83eef009f435
juandrewanz <juan@italic.com>
2023-07-14 19:11:59 -0300

    fixing counter

--- client/admin/src/components/counter.jsx
+++ client/admin/src/components/counter.jsx
@@ -1,56 +1,39 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Col, Row } from 'antd';
-import moment from 'moment';
+import { Col, Row } from "antd";
+import moment from "moment";
 
-const Counter = ({showCounterModal, url})=> {
+const Counter = ({
+  showCounterModal,
+  MTDCount,
+  currentShiftCount,
+  prevShiftCount,
+}) => {
+  const SHIFTS = {
+    morning: "Mañana",
+    afternoon: "Tarde",
+  };
 
-    const [MTDCount, setMTDCount] = useState(0);
-    const [currentShiftCount, setCurrentShiftCount] = useState(0);
-    const [prevShiftCount, setPrevShiftCount] = useState(0);
+  const hour = moment().hour();
+  const currentShift = hour < 14 ? SHIFTS.morning : SHIFTS.afternoon;
 
-    const SHIFTS = {
-        morning: "Mañana",
-        afternoon: "Tarde",
-      };
-
-      const hour = moment().hour();
-      const currentShift = hour < 14 ? SHIFTS.morning : SHIFTS.afternoon;
-
-      const endPoint = `${url}/key/counter`;
-
-    useEffect(()=> {
-       if(showCounterModal){ const countKeys = async () =>{
-            let { data: { MTDCount, currentShiftCount, prevShiftCount } } = await axios.get(endPoint);
-            setMTDCount(MTDCount);
-            setCurrentShiftCount(currentShiftCount);
-            setPrevShiftCount(prevShiftCount);
-        }
-        countKeys().catch(e => console.log(e))
-}
-    },[showCounterModal, endPoint])
-
-    return (
-        <Row className="mt-2 flex flex-col justify-center items-center border-2 border-blue-200 rounded-lg">
-            {/* <Col className='text-sm font-semibold'>Aforo Acumulado</Col> */}
-            <Row className='m-2 text-sm font-semibold'>Turno: {currentShift}</Row>    
-            <Row>           
-                <Col className="m-4 flex flex-col justify-center items-center">
-                    <Row className='text-sm font-medium'>Turno actual</Row>
-                    <Row>{currentShiftCount}</Row>
-                </Col>
-                <Col className="mx-4 flex flex-col justify-center items-center">
-                    <Row className='text-sm font-medium'>Turno anterior</Row>
-                    <Row>{prevShiftCount}</Row>
-                </Col>
-                <Col className="mx-4 flex flex-col justify-center items-center">
-                    <Row className='text-sm font-medium'>Mes</Row>
-                    <Row>{MTDCount}</Row>
-                </Col>
-            </Row>
-
-        </Row>
-    )
-}
+  return (
+    <Row className='mt-2 flex flex-col justify-center items-center border-2 border-blue-200 rounded-lg'>
+      <Row className='m-2 text-sm font-semibold'>Turno: {currentShift}</Row>
+      <Row>
+        <Col className='m-4 flex flex-col justify-center items-center'>
+          <Row className='text-sm font-medium'>Turno actual</Row>
+          <Row>{currentShiftCount}</Row>
+        </Col>
+        <Col className='mx-4 flex flex-col justify-center items-center'>
+          <Row className='text-sm font-medium'>Turno anterior</Row>
+          <Row>{prevShiftCount}</Row>
+        </Col>
+        <Col className='mx-4 flex flex-col justify-center items-center'>
+          <Row className='text-sm font-medium'>Mes</Row>
+          <Row>{MTDCount}</Row>
+        </Col>
+      </Row>
+    </Row>
+  );
+};
 
 export default Counter;
