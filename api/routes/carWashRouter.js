137e08a43e15cc6c5780e4e6dd60377cd40417af
juandrewanz <juan@italic.com>
2023-07-29 10:20:06 -0300

    making controllers generic

--- api/routes/carWashRouter.js
+++ api/routes/carWashRouter.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const {
-  checkCarWashKey,
-  generateRandomKey,
   generateKey,
   countGeneratedKeys,
   bringKeyCreationHisotry,
+  checkKey,
 } = require("../controllers");
 
 const carWashRouter = Router();
+const { Carwash } = require("../db");
 
 module.exports = carWashRouter;
 
@@ -15,7 +15,7 @@ carWashRouter.get("/:key", async (req, res) => {
   const { key } = req.params;
   console.log(key);
   try {
-    if (await checkCarWashKey(key)) {
+    if (await checkKey(key, Carwash)) {
       res.status(200).send("correct password");
     } else {
       res.status(404).send("password not found");
@@ -28,7 +28,7 @@ carWashRouter.get("/:key", async (req, res) => {
 carWashRouter.get("/key/generate", async (req, res) => {
   console.log("generando clave");
   try {
-    const key = await generateKey();
+    const key = await generateKey(Carwash);
     console.log("la clave es", key);
     if (key) {
       res.status(200).send({ key });
@@ -43,7 +43,7 @@ carWashRouter.get("/key/generate", async (req, res) => {
 carWashRouter.get("/key/counter", async (req, res) => {
   try {
     const { MTDCount, currentShiftCount, prevShiftCount } =
-      await countGeneratedKeys();
+      await countGeneratedKeys(Carwash);
     res.status(200).send({ MTDCount, currentShiftCount, prevShiftCount });
   } catch (e) {
     res.status(404).json({ error: e.message });
@@ -52,7 +52,7 @@ carWashRouter.get("/key/counter", async (req, res) => {
 
 carWashRouter.get("/key/history", async (req, res) => {
   try {
-    const keyHistory = await bringKeyCreationHisotry();
+    const keyHistory = await bringKeyCreationHisotry(Carwash);
     if (keyHistory) {
       res.status(200).send({ keyHistory });
     } else {
